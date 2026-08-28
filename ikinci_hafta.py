14bd27e08e3049bbc153bdd7cff7578c40a2c95a
Şimal Türkyılmaz <simaltrkylmz@gmail.com>
2026-08-28 14:46:42 +0300

    görev 4 ve genel düzenlemeler

--- ikinci_hafta.py
+++ ikinci_hafta.py
@@ -1,4 +1,5 @@
 import math
+import torch #görev 4 eklemesi
 #görev 1'i ve 2'yi graphviz ile çizdirme
 from graphviz import Digraph
 def trace(root):
@@ -45,6 +46,7 @@ class Value:
         return f"Value(data= {self.data},{self._op})"
 
     def __add__(self, other): #toplama için fonksiyon
+        other=other if isinstance(other,Value) else Value(other) #görev 4 eklemesi. Value objesi değilse onu Value'ya sarıyor.
         out= Value(self.data+other.data, (self,other),"+")
 
         #görev 3 eklemesi
@@ -52,22 +54,23 @@ class Value:
             self.grad += 1.0 * out.grad
             other.grad += 1.0 * out.grad
             """bir node ağaçta birden fazla yere gidiyorsa toplayarak ilerlemeli.
+            videoda da bu örneği verip bu kısmı düzeltmişti.
             mesela a=value(3.0). b=a+a. burada a hem self hem other. 
             self.grad += 1.0 * out.grad   # a.grad: 0 → 1
             other.grad += 1.0 * out.grad  # a.grad: 1 → 2 bu kısmı anlamak için yapay zekaya sordum."""
         out._backward = _backward
         return out
 
-    def __mul__(self, other): #çarpma için fonksiyon
-        out= Value(self.data*other.data,(self,other),"*")
 
+    def __mul__(self, other): #çarpma için fonksiyon
+        other = other if isinstance(other, Value) else Value(other)
+        out= Value(self.data*other.data,(self,other),"*")
         #görev 3 eklemesi
         def _backward():
             self.grad += other.data * out.grad
             other.grad += self.data * out.grad
         out._backward = _backward
         return out
-
     def tanh(self): #tanh fonksiyonu oluşturuyoruz
         x = self.data
         t = (math.exp(2 * x) - 1) / (math.exp(2 * x) + 1)
@@ -104,6 +107,37 @@ class Value:
         for node in reversed(topo):
             node._backward()
 
+    def __pow__(self, other): #görev 4 eklemesi: üs için
+        assert isinstance(other, (int, float)), "sadece int/float üsler destekleniyor"
+        out = Value(self.data ** other, (self,), f'**{other}')
+
+        def _backward():
+            self.grad += (other * self.data ** (other - 1)) * out.grad #türevinde üs başa çarpım olarak gelir ve bir azalır.
+        out._backward = _backward
+        return out
+
+    def __radd__(self,other): #görev 4 eklemesi: sıraları değişikse yerlerini değiştirip tekrar toplamaya çalışyıor.
+        return self+other
+
+    def __rmul__(self,other): #görev 4 eklemesi: sıraları değişikse yerlerini değiştirip tekrar çarpmaya çalışıyor.
+        return self*other
+
+    def __truediv__(self, other): #görev 4 eklemesi: bölme işlemi. yani aslında ikincinin -1. kuvvetiyle çarpma
+        return self* other**-1
+
+    def __sub__(self, other): #görev 4 eklemesi: çıkarma işlemi. aslında ikincinin işaretini negatif yapıp toplama
+        return self+(-other)
+
+    def __neg__(self): #görev 4 eklemesi: işaret değiştirme
+        return self*(-1)
+
+    def exp(self): #görev 4 eklemesi
+        x = self.data
+        out= Value(math.exp(x),(self,),"exp")
+        def _backward():
+            self.grad+=out.data * out.grad #e^x in türevi e^x. onu da out'ta hesapladığımız için out.data*out.grad diyoruz.
+        out._backward = _backward
+        return out
 
 #daha rahat ayrılsın diye görevleri fonksiyon olarak tanımladım.
 
@@ -243,16 +277,122 @@ def gorev3():
     grafik = draw_dot(o)
     grafik.render('grafikler/neuron_graph2', view=True)
 
+def gorev4():
+    #görev 3'teki girdi kısımlarını kopyala yapıştır yapıyoruz. tanh'i aşağıda parçalayacağız.
+    x1 = Value(2.0, label='x1')
+    x2 = Value(0.0, label='x2')
+
+    # weightler
+    w1 = Value(-3.0, label='w1')
+    w2 = Value(1.0, label='w2')
+
+    # bias (Karpathy videoda bu değeri yazmıştı)
+    b = Value(6.8813735870195432, label='b')
+
+    # çarpıp toplama işlemi
+    x1w1 = x1 * w1;
+    x1w1.label = 'x1*w1'
+    x2w2 = x2 * w2;
+    x2w2.label = 'x2*w2'
+    x1w1x2w2 = x1w1 + x2w2;
+    x1w1x2w2.label = 'x1*w1 + x2*w2'
+    n = x1w1x2w2 + b;
+    n.label = 'n'  # nöronun bias eklenmiş hali
+
+
+    #görev 4 eklemesi: tanh'ı parçalama
+    #tanh= (e**2x -1) / (e**2x +1)
+    e= (2*n).exp() #e**2x'i iki kere kullanacağımız için bir intermediate variable oluşturuyoruz.
+    o= (e-1) / (e+1)
+    o.label = 'o'
+    o.backward()
+    grafik = draw_dot(o)
+    grafik.render('grafikler/neuron_graph3', view=True)
 
 
 
+def gorev4_dogrulama(): #backward fonksiyonuyla hesapladığımız gradyanları pytorch kullanarak ve numerical derivative kullanarak da hesaplayıp karşılaştırıyoruz.
+    x1 = torch.Tensor([2.0]).double() #benim yazdığım x1=Value(2.0)
+    x1.requires_grad = True
+    x2 = torch.Tensor([0.0]).double()
+    x2.requires_grad = True
+    w1 = torch.Tensor([-3.0]).double()
+    w1.requires_grad = True
+    w2 = torch.Tensor([1.0]).double()
+    w2.requires_grad = True
+    b = torch.Tensor([6.8813735870195432]).double()
+    b.requires_grad = True
 
-"""gorev1()
+    n = x1 * w1 + x2 * w2 + b
+    o = torch.tanh(n) #benim yazdığım o=n.tanh()
+
+    print(o.data.item())
+    o.backward()
+
+    print('x2', x2.grad.item()) #benim yazdığım print(x2.grad)
+    print('w2', w2.grad.item())
+    print('x1', x1.grad.item())
+    print('w1', w1.grad.item())
+    print ("------ PyTorch sonu ------")
+
+    print("Benim yaptığım backward() ile hesaplama")
+
+    mx1= Value(2.0)
+    mx2=Value(0.0)
+    mw1=Value(-3.0)
+    mw2=Value(1.0)
+    mb=Value(6.8813735870195432)
+
+    mn= mx1*mw1 + mx2*mw2+mb
+    me= (mn*2).exp()
+    mo= (me-1)/(me+1)
+    mo.backward()
+
+    print(f"o değeri: {mo.data}")
+    print(f"x2: {mx2.grad}")
+    print(f"w2: {mw2.grad}")
+    print(f"x1: {mx1.grad}")
+    print(f"w1: {mw1.grad}")
+    print("------ Backward fonksiyonuyla hesaplama sonu -------")
+
+    print("Numerical derivative ile hesaplama")
+
+    h=0.0001
+    nx1= Value(2.0)
+    nx2= Value(0.0)
+    nw1= Value(-3.0)
+    nw2= Value(1.0)
+    nb= Value(6.8813735870195432)
+    nn1= nx1*nw1 + nx2*nw2+nb
+    ne1=(nn1*2).exp()
+    no1= (ne1-1)/(ne1+1)
+    L1= no1.data
+
+    nx1h= Value(2.0)
+    nx2h= Value(0.0)
+    nw1h= Value(-3.0+h)
+    nw2h= Value(1.0)
+    nbh= Value(6.8813735870195432)
+    nn2= nx1h*nw1h + nx2h*nw2h+nbh
+    ne2=(nn2*2).exp()
+    no2= (ne2-1)/(ne2+1)
+    L2= no2.data
+
+    num_grad= (L2-L1)/h
+
+    print(f"w1'in gradyanı: {num_grad}")
+
+#gorev1()
+#gorev2_1()
+#gorev2_2()
+#gorev3()
+#gorev4()
+#gorev4_dogrulama()
+
+gorev1()
 gorev2_1()
-gorev2_2()"""
+gorev2_2()
 gorev3()
-
-
-
-
+gorev4()
+gorev4_dogrulama()
 
