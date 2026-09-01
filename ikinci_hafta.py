9e425b32b8ead11497af9099fd3c26b1bfe0e194
Şimal Türkyılmaz <simaltrkylmz@gmail.com>
2026-09-01 13:37:29 +0300

    gorev 5 elle loss yazdırma

--- ikinci_hafta.py
+++ ikinci_hafta.py
@@ -445,11 +445,26 @@ def gorev5():
             for layer in self.layers:
                 params+=layer.parameters()
             return params
-    x=[2.0, 3.0, -1.0]
-    n=MLP(3,[4,4,1])
-    grafik = draw_dot(n(x))
-    grafik.render('grafikler/mlpgraph', view=True)
 
+    """grafik = draw_dot(n(x))
+    grafik.render('grafikler/mlpgraph', view=True)"""
+    n=MLP(3,[4,4,1])
+    xs = [
+        [2.0, 3.0, -1.0],
+        [3.0, -1.0, 0.5],
+        [0.5, 1.0, 1.0],
+        [1.0, 1.0, -1.0]
+    ]
+    ys = [1.0, -1.0, -1.0, 1.0]  # Ağdan bulmasını istediğimiz doğru cevaplar
+    n = MLP(3, [4, 4, 1])
+    ypred=[n(x) for x in xs]
+    loss_sum = sum((yout - ygt) ** 2 for ygt, yout in zip(ys, ypred))
+
+    for ygt, yout in zip (ys, ypred):
+        loss= (yout - ygt)**2
+        print (loss.data)
+
+    print("Güncel Hata (Loss):", loss_sum.data)
 
 gorev5()
 
