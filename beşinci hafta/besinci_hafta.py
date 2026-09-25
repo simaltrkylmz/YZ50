2c81772fdaa289bf266a2f6e4b22a3aedf05965c
Şimal Türkyılmaz <simaltrkylmz@gmail.com>
2026-09-25 23:35:47 +0300

    beşinci hafta

--- beşinci hafta/besinci_hafta.py
+++ beşinci hafta/besinci_hafta.py
@@ -4,8 +4,8 @@ print("""-----------------------
 -----------------------""")
 
 import torch
-import torch.nn.functional as F
 import random
+import torch.nn.functional as F
 
 #veri seti hazırlığı (geçen haftaların aynısı)
 words = open('names.txt', 'r').read().splitlines()
@@ -109,4 +109,49 @@ for t in [logprobs, probs, counts_sum_inv, counts_sum, counts,
 
 loss.backward()
 print(f"PyTorch Loss: {loss.item():.4f}")
+
+#elle türev hesapları
+
+#formülleri hatırlamak için üstlerine yazdım.
+
+#loss = -logprobs[range(n), Yb].mean()
+dlogprobs= torch.zeros_like(logprobs) #logprobs ile aynı boyutta sıfırlarla dolu matris
+dlogprobs[range(n), Yb]=-1.0/n #logprobstaki sayılarla oynarsak loss'u nasıl etkilerler
+cmp("logprobs", dlogprobs,logprobs) #kontrol fonksiyonu
+
+#logprobs = probs.log()
+dprobs= (1.0/probs) * dlogprobs #dloss/dprobs= (dloss/dlogprobs) * (dlogprobs/dprobs) : zincir kuralı
+cmp("probs", dprobs,probs)
+
+#probs = counts * counts_sum_inv
+dcounts_sum_inv= (counts * dprobs).sum(1, keepdim=True)
+#forward pass'te counts_sum_inv 30 harfe de etki etmesi için pytorch tarafından 32x30 boyutuna genişletiliyor. bu yüzden geri dönerken 30 farklı koldan gelen etkiyi tek sütunda topluyoruz.
+cmp("counts_sum_inv", dcounts_sum_inv,counts_sum_inv)
+
+#counts_sum_inv = counts_sum**-1
+dcounts_sum= -counts_sum**-2 * dcounts_sum_inv
+cmp("counts_sum", dcounts_sum,counts_sum)
+
+#probs = counts * counts_sum_inv
+#counts_sum = counts.sum(1, keepdims=True) #toplamın türevi 1
+#counts iki ayrı yerde. bu yüzden geriye dönerken bu iki yoldan gelen türevleri toplamamız gerekiyor.
+dcounts= counts_sum_inv * dprobs
+dcounts+= torch.ones_like(counts) * dcounts_sum
+cmp("counts", dcounts,counts)
+
+#counts = norm_logits.exp()
+dnorm_logits = dcounts * counts #e^x'in türevi yine kendisidir. bu da counts değişkeni içinde
+cmp("norm_logits", dnorm_logits,norm_logits)
+
+#norm_logits = logits - logit_maxes
+dlogit_maxes= (-1 * dnorm_logits).sum(1,keepdim=True) #yine genişletme
+cmp('logit_maxes', dlogit_maxes, logit_maxes)
+
+#norm_logits = logits - logit_maxes
+#logit_maxes = logits.max(1, keepdim=True).values
+#sadece en yüksek değerli harfi içeri alıyoruz, kalanların etkisi yok.
+dlogits= 1 * dnorm_logits
+dlogits+= F.one_hot(logits.max(1).indices, num_classes=logits.shape[1])*dlogit_maxes
+cmp('logits', dlogits,logits)
+
 
